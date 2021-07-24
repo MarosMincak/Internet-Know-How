8bbaf25960c88c50724c309d819d2bef49d825b6
Maroš Minčák <maros.mincak11@gmail.com>
2021-07-24 10:56:09 +0200

    initial commit

---
+++ book/views.py
@@ -0,0 +1,199 @@
+from django.shortcuts import render
+import random
+
+
+# Create your views here.
+
+def domov(request):
+    return render(request, 'book/home.html', context={'text_next': 'Obsah >'})
+
+
+def obsah(request):
+    return render(request, 'book/obsah.html',
+                  context={'text_previous': '< Domov',
+                           'text_next': '1. Internet - základ novodobého života? >',
+                           'title': 'Obsah'})
+
+
+def one(request):
+    return render(request, 'kapitoly/one/one.html', context={
+        'text_previous': '< Obsah',
+        'text_next': '2. Digitalizácia ľudstva >',
+        'title': '1 Internet - základ novodobého ľudstva?'})
+
+
+def two(request):
+    return render(request, 'kapitoly/two/two.html', context={
+        'text_previous': '< 1 Internet - základ novodobého ľudstva?',
+        'text_next': '2.1 Kybernetická bezpečnosť >',
+        'title': '2 Digitalizácia ľudstva'})
+
+
+def two_one(request):
+    return render(request, 'kapitoly/two/two-one.html', context={
+        'text_previous': '< 2 Digitalizácia ľudstva',
+        'text_next': '2.2 Kybernetické útoky >',
+        'title': '2.1 Kybernetická bezpečnosť'})
+
+
+def two_two(request):
+    return render(request, 'kapitoly/two/two-two.html', context={
+        'text_previous': '< 2.1 Kybernetická bezpečnosť',
+        'text_next': '3 Hrozby internetu >',
+        'title': '2.2 Kybernetické útoky'})
+
+
+def three(request):
+    return render(request, 'kapitoly/three/three.html', context={
+        'text_previous': '< 2.2 Kybernetické útoky',
+        'text_next': '3.1 Bezpečné používanie internetu >',
+        'title': '3 Hrozby internetu'})
+
+
+def three_one(request):
+    return render(request, 'kapitoly/three/three-one.html', context={
+        'text_previous': '< 3 Hrozby internetu',
+        'text_next': '3.1.1 Tvorba hesla >',
+        'title': '3.1 Bezpečné používanie internetu'})
+
+def three_one_one(request):
+    thepassword = ''
+    length = int(request.GET.get('length', 10))
+    characters = list('abcdefghijklmnoprstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPRSTUVWXYZ'))
+    if request.GET.get('specialcharacters'):
+        characters.extend(list('+`@#$~^&*{}°^¨;–><'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'kapitoly/three/three-one-one.html', context={
+        'text_previous': '< 3.1 Bezpečné používanie internetu',
+        'text_next': '3.1.2 Ukladanie hesla >',
+        'title': '3.1.1 Tvorba hesla',
+        'password': thepassword})
+
+def three_one_two(request):
+    return render(request, 'kapitoly/three/three-one-two.html', context={
+        'text_previous': '< 3.1.1 Tvorba hesla',
+        'text_next': '3.1.3 Verejné siete (WIFI) >',
+        'title': '3.1.2 Ukladanie hesla'})
+
+
+def three_one_three(request):
+    return render(request, 'kapitoly/three/three-one-three.html', context={
+        'text_previous': '< 3.1.2 Ukladanie hesla',
+        'text_next': '3.2 Sociálne siete >',
+        'title': '3.1.3 Verejné siete (WIFI)'})
+
+
+def three_two(request):
+    return render(request, 'kapitoly/three/three-two.html', context={
+        'text_previous': '< 3.1.3 Verejné siete (WIFI)',
+        'text_next': '3.2.1 Pár základných tipov k bezpečnému používaniu sociálnych sietí >',
+        'title': '3.2 Sociálne siete'})
+
+
+def three_two_one(request):
+    return render(request, 'kapitoly/three/three-two-one.html', context={
+        'text_previous': '< 3.2 Sociálne siete',
+        'text_next': '3.3 Počítačové vírusy a spam >',
+        'title': '3.2.1 Pár základných tipov k bezpečnému používaniu sociálnych sietí'})
+
+
+def three_three(request):
+    return render(request, 'kapitoly/three/three-three.html', context={
+        'text_previous': '< 3.2.1 Pár základných tipov k bezpečnému používaniu sociálnych sietí',
+        'text_next': '3.3.1 Najbežnejšie typy počítačových vírusov >',
+        'title': '3.3 Počítačové vírusy a spam'})
+
+
+def three_three_one(request):
+    return render(request, 'kapitoly/three/thre-thre-one.html', context={
+        'text_previous': '< 3.3 Počítačové vírusy a spam',
+        'text_next': '3.3.2 Mylné predstavy o počítačových vírusoch >',
+        'title': '3.3.1 Najbežnejšie typy počítačových vírusov'})
+
+
+def three_three_two(request):
+    return render(request, 'kapitoly/three/thre-thre-two.html', context={
+        'text_previous': '< 3.3.1 Najbežnejšie typy počítačových vírusov',
+        'text_next': '3.3.3 Ako (ne)dostať počítačový vírus >',
+        'title': '3.3.2 Mylné predstavy o počítačových vírusoch'})
+
+
+def three_three_three(request):
+    return render(request, 'kapitoly/three/tre-tre-tre.html', context={
+        'text_previous': '< 3.3.2 Mylné predstavy o počítačových vírusoch',
+        'text_next': '3.3.4 Ako identifikovať a zneškodniť počítačový vírus >',
+        'title': '3.3.3 Ako (ne)dostať počítačový vírus'})
+
+
+def three_three_four(request):
+    return render(request, 'kapitoly/three/tre-tre-four.html', context={
+        'text_previous': '< 3.3.3 Ako (ne)dostať počítačový vírus',
+        'text_next': '3.4 Spam >',
+        'title': '3.3.4 Ako identifikovať a zneškodniť počítačový vírus'})
+
+
+def three_four(request):
+    return render(request, 'kapitoly/three/three-four.html', context={
+        'text_previous': '< 3.3.4 Ako identifikovať a zneškodniť počítačový vírus',
+        'text_next': '3.4.1 Phishing >',
+        'title': '3.4 Spam'})
+
+
+def three_four_one(request):
+    return render(request, 'kapitoly/three/three-four-one.html', context={
+        'text_previous': '< 3.4 Spam',
+        'text_next': '3.4.2 Ako sa brániť proti Spamu a Phishingu >',
+        'title': '3.4.1 Phishing'})
+
+
+def three_four_two(request):
+    return render(request, 'kapitoly/three/three-four-two.html', context={
+        'text_previous': '< 3.4.1 Phishing',
+        'text_next': '3.4.3 Sociálne inžinierstvo >',
+        'title': '3.4.2 Ako sa brániť proti Spamu a Phishingu'})
+
+
+def three_four_three(request):
+    return render(request, 'kapitoly/three/thre-four-thre.html', context={
+        'text_previous': '< 3.4.2 Ako sa brániť proti Spamu a Phishingu',
+        'text_next': '4 Netiketa >',
+        'title': '3.4.3 Sociálne inžinierstvo'})
+
+
+def four(request):
+    return render(request, 'kapitoly/four/four.html', context={
+        'text_previous': '< 3.4.3 Sociálne inžinierstvo',
+        'text_next': '4.1 Kyberšikana >',
+        'title': '4 Netiketa'})
+
+
+def four_one(request):
+    return render(request, 'kapitoly/four/four-one.html', context={
+        'text_previous': '< 4 Netiketa',
+        'text_next': '4.1.1 Ako sa kyberšikane vyvarovať a brániť >',
+        'title': '4.1 Kyberšikana'})
+
+
+def four_one_one(request):
+    return render(request, 'kapitoly/four/four-one-one.html', context={
+        'text_previous': '< 4.1 Kyberšikana',
+        'text_next': '5. Dvojjazyčný výkladový slovník použitých pojmov >',
+        'title': '4.1.1 Ako sa kyberšikane vyvarovať a brániť'})
+
+
+def five(request):
+    return render(request, 'kapitoly/five/five.html', context={
+        'text_previous': '< 4.1.1 Ako sa kyberšikane vyvarovať a brániť',
+        'text_next': 'Zoznam použitých zdrojov >',
+        'title': '5. Dvojjazyčný výkladový slovník použitých pojmov'})
+
+
+def zoznam_pouzitych_zdrojov(request):
+    return render(request, 'book/zoz-pouz-zdroj.html', context={
+        'text_previous': '< 5. Dvojjazyčný výkladový slovník použitých pojmov',
+        'title': 'Zoznam použitých zdrojov'})
---
+++ internet_know_how/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from book import views
+
+urlpatterns = [
+    path('', views.domov, name='domov'),
+    path('obsah', views.obsah, name='obsah'),
+    path('internet-zaklad-novodobeho-zivota', views.one, name='one'),
+    path('digitalizacia-ludstva', views.two, name='two'),
+    path('kyberneticka-bezpecnost', views.two_one, name='two_one'),
+    path('kyberneticke-utoky', views.two_two, name='two_two'),
+    path('hrozby-internetu', views.three, name='three'),
+    path('bezpecne-pouzivanie-internetu', views.three_one, name='three_one'),
+    path('tvorba-hesla', views.three_one_one, name='three_one_one'),
+    path('ukladanie-hesla', views.three_one_two, name='three_one_two'),
+    path('verejne-siete-wifi', views.three_one_three, name='three_one_three'),
+    path('socialne-siete', views.three_two, name='three_two'),
+    path('tipy-k-bezpecnemu-pouzivaniu-socialnych-sieti', views.three_two_one, name='three_two_one'),
+    path('pocitacove-virusy-a-spam', views.three_three, name='three_three'),
+    path('najbeznejsie-typy-pocitacovych-virusov', views.three_three_one, name='three_three_one'),
+    path('mylne-predstavy-o-pocitacovych-virusoch', views.three_three_two, name='three_three_two'),
+    path('ako-nedostat-pocitacovy-virus', views.three_three_three, name='three_three_three'),
+    path('ako-identifikovat-a-zneskodnit-pocitacovy-virus', views.three_three_four, name='three_three_four'),
+    path('spam', views.three_four, name='three_four'),
+    path('phishing', views.three_four_one, name='three_four_one'),
+    path('ako-sa-branit-proti-spamu-a-phishingu', views.three_four_two, name='three_four_two'),
+    path('socialne-inzinierstvo', views.three_four_three, name='three_four_three'),
+    path('netiketa', views.four, name='four'),
+    path('kybersikana', views.four_one, name='four_one'),
+    path('ako-sa-kybersikane-vyvarovat-a-branit', views.four_one_one, name='four_one_one'),
+    path('dvojjazycny-vykladovy-slovnik-pouzitych-pojmov', views.five, name='five'),
+    path('zoznam-pouzitych-zdrojov', views.zoznam_pouzitych_zdrojov, name='zoznam_pouzitych_zdrojov'),
+]
